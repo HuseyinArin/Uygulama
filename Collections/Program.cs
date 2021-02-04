44a7c42902ed877d0955b5f5419c23af6c622607
HuseyinArin <78267136+HuseyinArin@users.noreply.github.com>
2021-02-04 15:06:27 +0300

    16 Ocak ders sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"hüseyin",
+            //    "bilge","aslan","bilghüs" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //isimler = new string[5];
+            //isimler[4] = "ilker";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+
+            List<string> isimler2 = new List<string> { "hüseyin",
+            "bilge","aslan","bilghüs"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("ilker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+            List<string> Liste = new List<string>();
+            Console.WriteLine(Liste.Count);
+        }
+    }
+
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //generic oldugu için calişmasına izi veremez
+            //bu yüzden ona ne ile çalişacağimizi soylemeliyiz.
+
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Hüseyin");
+
+
+
+        }
+    }
+}
