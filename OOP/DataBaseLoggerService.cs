e03040b0624f0c39a9e47bcd7c5d2099a319de40
HuseyinArin <78267136+HuseyinArin@users.noreply.github.com>
2021-02-07 20:36:25 +0300

    Add files via upload

---
+++ OOP/DataBaseLoggerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP4
+{
+    class DataBaseLoggerService : ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("Veritabanına loglandı");
+        }
+    }
+}
---
+++ OOP/KonutKrediManger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP4
+{
+    class KonutKrediManger : IKrediManager
+    {
+        public void BiseyYap()
+        {
+          
+        }
+
+        public void Hesapla()
+        {
+            Console.WriteLine("Konut kredisi ödeme planı hesaplandı");
+        }
+    }
+}
